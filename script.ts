b3221842e119e20e1452540136266858fdb4e450
Andy Chou <acchou2@gmail.com>
2017-08-07 21:08:49 -0700

    Add showNote example from javascript.info

--- script.ts
+++ script.ts
@@ -77,7 +77,7 @@ function annotateListItems() {
 
 annotateListItems();
 
-function sortTable(table: HTMLTableElement, sortColumn) {
+function sortTable(table: HTMLTableElement, sortColumn: number) {
     let sortedRows = Array.from(table.rows)
         .slice(1)
         .sort((rowA, rowB) => rowA.cells[sortColumn].textContent > rowB.cells[sortColumn].textContent ? 1 : -1);
@@ -85,7 +85,7 @@ function sortTable(table: HTMLTableElement, sortColumn) {
     let firstRow = table.rows[0];
 }
 
-function centerXY(elem, container) {
+function centerXY(elem: HTMLElement, container: Element) {
     let midY = container.clientHeight / 2;
     let midX = container.clientWidth / 2;
     let diameter = elem.offsetWidth;
@@ -102,7 +102,7 @@ document.onclick = function (e) { // shows click coordinates
     coords.innerHTML = e.clientX + ':' + e.clientY;
 };
 
-function getCoords(elem) {
+function getCoords(elem: Element) {
     let bounds = elem.getBoundingClientRect();
     let upperLeft = { x: bounds.left, y: bounds.top };
     let lowerRight = { x: bounds.right, y: bounds.bottom };
@@ -117,6 +117,101 @@ function getCoords(elem) {
     };
 }
 
-function positionAt(anchor, position, elem) {
-
+function positionAtFixed(anchor: Element, position: string, elem: HTMLElement) {
+    let anchorBounds = anchor.getBoundingClientRect();
+    let top, left;
+    switch (position) {
+        case "top":
+            top = anchorBounds.top - elem.offsetHeight;
+            left = anchorBounds.left;
+            break;
+        case "bottom":
+            top = anchorBounds.bottom;
+            left = anchorBounds.left;
+            break;
+        case "right":
+            top = anchorBounds.top;
+            left = anchorBounds.right;
+            break;
+        default:
+            console.log("bad position: " + position);
+            break;
+    }
+    elem.style.top = String(top) + "px";
+    elem.style.left = String(left) + "px";
 }
+
+function showNoteFixed(anchor: Element, position: string, html: string) {
+    let note = document.createElement("div");
+    note.classList.add("note");
+    note.innerHTML = html;
+    note.style.position = "fixed";
+    document.body.appendChild(note);
+    positionAtFixed(anchor, position, note);
+}
+
+function getAbsoluteCoords(elem: Element) {
+    let box = elem.getBoundingClientRect();
+
+    return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset,
+        bottom: box.bottom + pageYOffset,
+        right: box.right + pageXOffset
+    };
+}
+
+function positionAtAbsolute(anchor: Element, position: string, elem: HTMLElement) {
+    let anchorBounds = getAbsoluteCoords(anchor);
+    let top, left;
+    switch (position) {
+        case "top":
+            top = anchorBounds.top - elem.offsetHeight;
+            left = anchorBounds.left;
+            break;
+        case "bottom":
+            top = anchorBounds.bottom;
+            left = anchorBounds.left;
+            break;
+        case "right":
+            top = anchorBounds.top;
+            left = anchorBounds.right;
+            break;
+        case "top-in":
+            top = anchorBounds.top;
+            left = anchorBounds.left;
+            break;
+
+        case "bottom-in":
+            top = anchorBounds.bottom - elem.clientHeight;
+            left = anchorBounds.left;
+            break;
+
+        case "right-in":
+            top = anchorBounds.top;
+            left = anchorBounds.right - elem.clientWidth;
+        default:
+            console.log("bad position: " + position);
+            break;
+    }
+    elem.style.top = String(top) + "px";
+    elem.style.left = String(left) + "px";
+}
+
+function showNoteAbsolute(anchor: Element, position: string, html: string) {
+    let note = document.createElement("div");
+    note.classList.add("note");
+    note.innerHTML = html;
+    note.style.position = "absolute";
+    document.body.appendChild(note);
+    positionAtAbsolute(anchor, position, note);
+}
+
+let blockquote = document.querySelector("blockquote");
+showNoteAbsolute(blockquote, "top", "note above");
+showNoteAbsolute(blockquote, "right", "note at right");
+showNoteAbsolute(blockquote, "bottom", "note below");
+showNoteAbsolute(blockquote, "top-in", "note top in");
+showNoteAbsolute(blockquote, "bottom-in", "note bottom in");
+showNoteAbsolute(blockquote, "right-in", "note right in");
+document.body.style.height = "2000px";
