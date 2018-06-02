666f74b0e9c2c9096a14e34838c41f818f16dc88
Orhan Mekic <orhan@virtualobject.se>
2018-06-02 15:45:46 +0200

    add sidebar to all pages

---
+++ js/sidebar.js
@@ -0,0 +1,12 @@
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
